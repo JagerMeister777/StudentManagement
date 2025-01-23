34673d6b80e633a126460c9065d560ffa933954e
ast-shoji-001 <yuki.shoji@synchrotech.co.jp>
2025-01-23 13:03:57 +0900

    .gitignoreの作成とパスの変更とコードフォーマット

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -4,11 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -23,12 +19,12 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/student/list")
-	public List<Student> getStudentsList(){
+	@GetMapping("/students")
+	public List<Student> getStudentsList() {
     return repository.getStudentsList();
 	}
 
-	@GetMapping("/course/list")
+	@GetMapping("/courses")
 	public List<Course> getCoursesList() {
 		return repository.getCoursesList();
 	}
